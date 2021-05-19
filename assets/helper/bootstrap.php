187f27fa79f0720ba6775a01ce4f6383f59b62d9
jjenus <biz.jenus@gmail.com>
2021-05-19 08:59:58 +0100

    update

--- assets/helper/bootstrap.php
+++ assets/helper/bootstrap.php
@@ -3,7 +3,7 @@ if (!isset($_SESSION)) {
   session_start();
 }
 
-defined("ENVIRONMENT") || define('ENVIRONMENT', "development");
+defined("ENVIRONMENT") || define('ENVIRONMENT', "production");
 defined("APP_NAME") || define('APP_NAME', "Amanda's");
 defined("CURRENCY") || define('CURRENCY', "NGN ");
 defined("ROOTPATH") || define('ROOTPATH', $_SERVER["DOCUMENT_ROOT"].'/');
